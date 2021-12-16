dd5ea5c4ce6ea37a6083d078d4872778ccaad067
JoeChan <dr.elichan@yandex.com>
2021-12-16 12:03:59 +0800

    ...

--- runme.sh
+++ runme.sh
@@ -1,3 +1,4 @@
-git fetch upstream
-git checkout dpt_scriptable
-git pull upstream dpt_scriptable
+#git fetch upstream
+#git checkout dpt_scriptable
+#git pull upstream dpt_scriptable
+python3 export_model.py . weights/dpt_hybrid-midas-d889a10e.pt traced_model_002.pt
